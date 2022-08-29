d4654b4c880a010203067a101f430a859717ca97
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-30 04:09:51 +0600

    fixing bugs

--- ePathshala/src/HandleRequests/root/GetStudentDetails.cpp
+++ ePathshala/src/HandleRequests/root/GetStudentDetails.cpp
@@ -4,9 +4,9 @@ void GetStudentDetails(Json::Value &request, Json::Value &response, drogon::orm:
 {
     std::clog << "Get \"student-details\" request" << std::endl;
 
-    std::stringstream queryStream;
+    std::clog << request << std::endl;
 
-    queryStream.str("SELECT * FROM GET_STUDENT_DETAILS($1)");
+    std::stringstream queryStream("SELECT * FROM GET_STUDENT_DETAILS($1)");
 
     std::shared_future<drogon::orm::Result> resultFuture = dbClient.execSqlAsyncFuture(queryStream.str(), request["user_id"].asString());
 
