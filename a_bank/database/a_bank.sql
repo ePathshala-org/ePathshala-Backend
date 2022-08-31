12b89b3d87d477f448f15850188f318c1019f9ac
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-31 06:20:32 +0600

    Update a_bank.sql

--- a_bank/database/a_bank.sql
+++ a_bank/database/a_bank.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.3
--- Dumped by pg_dump version 14.3
+-- Dumped from database version 14.5
+-- Dumped by pg_dump version 14.5
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -15,6 +15,33 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- Name: add_credit(bigint, character varying, integer); Type: FUNCTION; Schema: public; Owner: a_bank
+--
+
+CREATE FUNCTION public.add_credit(param_client_id bigint, param_password character varying, param_amount integer) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+	SELECTED_CLIENT_ID BIGINT;
+	AMOUNT INT;
+BEGIN
+	SELECT CLIENT_ID INTO SELECTED_CLIENT_ID
+	FROM CLIENTS
+	WHERE CLIENT_ID = PARAM_CLIENT_ID AND SECURITY_KEY = PARAM_PASSWORD;
+	IF SELECTED_CLIENT_ID IS NULL THEN
+		RETURN 1; --INVALID CREDENTIALS
+	END IF;
+	UPDATE CLIENTS
+	SET CREDIT = CREDIT + PARAM_AMOUNT
+	WHERE CLIENT_ID = PARAM_CLIENT_ID;
+	RETURN 0; --SUCCESS
+END;
+$$;
+
+
+ALTER FUNCTION public.add_credit(param_client_id bigint, param_password character varying, param_amount integer) OWNER TO a_bank;
 
 --
 -- Name: subtract(bigint, character varying, integer); Type: FUNCTION; Schema: public; Owner: a_bank
@@ -74,10 +101,6 @@ ALTER TABLE public.clients OWNER TO a_bank;
 --
 
 COPY public.clients (client_id, security_key, credit) FROM stdin;
-8	12345678	100000
-9	12345678	100000
-13	12345678	100000
-14	12345678	100000
 15	12345678	100000
 18	12345678	100000
 21	12345678	100000
@@ -96,7 +119,11 @@ COPY public.clients (client_id, security_key, credit) FROM stdin;
 49	12345678	100000
 54	12345678	100000
 56	12345678	100000
-2	12345678	99300
+9	12345678	0
+2	12345678	98100
+8	12345678	98000
+13	12345678	97600
+14	12345678	103200
 \.
 
 
